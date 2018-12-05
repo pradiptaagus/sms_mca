a55d139264b5c087caf3ede8339583ba878a512d
pradiptaagus <igedeaguspradipta@gmail.com>
2018-12-05 13:12:28 +0800

    error handling when database connection lost

--- my_websocket.py
+++ my_websocket.py
@@ -10,8 +10,9 @@ import time
 
 api_token = 'o.3jlS0nO1oAQ5bO3Nq7uo6jS899W9fFM4'
 api_base_url = 'wss://stream.pushbullet.com/websocket/'
-db = pymysql.connect("localhost", "root", "", "sms_mca")
-cur = db.cursor()
+
+
+    
 
 def on_message(ws, message):
     result = message = json.loads(message)
@@ -27,32 +28,64 @@ def on_message(ws, message):
         print("application_name: " + str(applicationName))
         print("title: " + str(messageTitle))
         print("message: " + str(messageBody))
-        print("\n")
     
     if messageType == "sms_changed":
         notifications = result.get("push",{}).get("notifications")
-        
-        messageBody = notifications[0].get("body")
-        
-        sender = notifications[0].get("title")
-        
-        source_device_iden = result.get("push",{}).get("source_device_iden")
-        
-        timestamp = notifications[0].get("timestamp")
-        readable_timestamp = datetime.datetime.fromtimestamp(timestamp)
-        readable_timestamp = readable_timestamp.strftime('%Y-%m-%d %H:%M:%S')
+
+        if not notifications:
+            # do nothing
+            print()
+        else:
+            messageBody = notifications[0].get("body")
+            
+            sender = notifications[0].get("title")
+            
+            source_device_iden = result.get("push",{}).get("source_device_iden")
+            
+            timestamp = notifications[0].get("timestamp")
+            readable_timestamp = datetime.datetime.fromtimestamp(timestamp)
+            readable_timestamp = readable_timestamp.strftime('%Y-%m-%d %H:%M:%S')
 
 
-        # insert response to database
-        query = "INSERT INTO inbox (source_device_iden, sender, message, status, inbox_time) VALUES('%s', '%s', '%s', 1, '%s');" %(source_device_iden, sender, messageBody, readable_timestamp)
-        cur.execute(query)
-        db.commit()
+            # insert response to database
+            try:
+                db = pymysql.connect("localhost", "root", "", "sms_mca")
+                cur = db.cursor()
+                
+                query = "INSERT INTO inbox (source_device_iden, sender, message, status, inbox_time) VALUES('%s', '%s', '%s', 1, '%s');" %(source_device_iden, sender, messageBody, readable_timestamp)
+                cur.execute(query)
+                db.commit()
 
-        print("type: " + str(messageType))
-        print("source_device_iden: " + str(source_device_iden))
-        print("sender: " + str(sender))
-        print("message: " + str(messageBody))
-        print("timestamp: " + str(readable_timestamp))
+                print("type: " + str(messageType))
+                print("source_device_iden: " + str(source_device_iden))
+                print("sender: " + str(sender))
+                print("message: " + str(messageBody))
+                print("timestamp: " + str(readable_timestamp))
+            
+            except pymysql.DataError as e:
+                error = "DataError: " + str(e)
+                print(error)
+
+            except pymysql.InternalError as e:
+                error = "InternalError: " + str(e)
+                print(error)
+            
+            except pymysql.IntegrityError as e:
+                error = "IntegrityError: " + str(e)
+                print(error)
+
+            except pymysql.OperationalError as e:
+                error = "OperationalError: " + str(e)
+                print(error)
+
+            except pymysql.NotSupportedError as e:
+                error = "NotSupportedError: " + str(e)
+                print(error)
+
+            except pymysql.ProgrammingError as e:
+                error = "ProgrammingError: " + str(e)
+                print(error)
+
         print("\n")
 
     else:
--- reply_engine.py
+++ reply_engine.py
@@ -1,18 +1,44 @@
 import pymysql
 import json
 import requests
+import time
 
 api_token = 'o.3jlS0nO1oAQ5bO3Nq7uo6jS899W9fFM4'
 api_base_url = 'https://api.pushbullet.com/v2/'
 headers = {'Content-Type': 'application/json', 'Authorization': 'Bearer {0}'.format(api_token)}
 
-db = pymysql.connect("localhost", "root", "", "sms_mca")
-cur = db.cursor()
-
 def getOutbox():
-    query = "SELECT * FROM outbox;"
-    cur.execute(query)
-    result = cur.fetchall()
+    result = ""
+    
+    try: 
+        query = "SELECT * FROM outbox;"
+        cur.execute(query)
+        result = cur.fetchall()
+
+    except pymysql.DataError as e:
+        error = "DataError: " + str(e)
+        print(error)
+
+    except pymysql.InternalError as e:
+        error = "InternalError: " + str(e)
+        print(error)
+    
+    except pymysql.IntegrityError as e:
+        error = "IntegrityError: " + str(e)
+        print(error)
+
+    except pymysql.OperationalError as e:
+        error = "OperationalError: " + str(e)
+        print(error)
+
+    except pymysql.NotSupportedError as e:
+        error = "NotSupportedError: " + str(e)
+        print(error)
+
+    except pymysql.ProgrammingError as e:
+        error = "ProgrammingError: " + str(e)
+        print(error)
+
     return result
 
 def reply(target_device_iden, source_user_iden, conversation_iden, message):
@@ -36,19 +62,82 @@ def reply(target_device_iden, source_user_iden, conversation_iden, message):
         return json.loads(response.content.decode('utf-8'))
     else:
         return None
+
+def updateSatus(message_id):
+    try:
+        query = "UPDATE outbox SET status = 1 WHERE outbox_id = %s;" %(message_id)
+        cur.execute(query)
+        db.commit()
+        print("status updated!")
+
+    except pymysql.DataError as e:
+        error = "DataError: " + str(e)
+        print(error)
+
+    except pymysql.InternalError as e:
+        error = "InternalError: " + str(e)
+        print(error)
     
+    except pymysql.IntegrityError as e:
+        error = "IntegrityError: " + str(e)
+        print(error)
+
+    except pymysql.OperationalError as e:
+        error = "OperationalError: " + str(e)
+        print(error)
+
+    except pymysql.NotSupportedError as e:
+        error = "NotSupportedError: " + str(e)
+        print(error)
+
+    except pymysql.ProgrammingError as e:
+        error = "ProgrammingError: " + str(e)
+        print(error)
 
 def main():
-    allData = getOutbox()
-    for i in allData:
-        if i[5] == 0:
-            target_device_iden = i[1]
-            source_user_iden = i[2]
-            conversation_iden = i[3]
-            message = i[4]
+    try: 
+        db = pymysql.connect("localhost", "root", "", "sms_mca")
+        cur = db.cursor()
+        allData = getOutbox()
+        
+        if allData == "":
+            print("Failed to conect to database!")
+        else:
+            for i in allData:
+                if i[5] == 0:
+                    message_id = i[0]
+                    target_device_iden = i[1]
+                    source_user_iden = i[2]
+                    conversation_iden = i[3]
+                    message = i[4]
 
-            print(target_device_iden + ", "+ source_user_iden + ", "+ conversation_iden + ", "+ message)
+                    print("Send message:")
+                    print("target_device_iden: " + target_device_iden)
+                    print("source_user_iden: " + source_user_iden)
+                    print("conversation_iden: " + conversation_iden)
+                    print("message: " + message)
+                    print("\n")
+                    
+                    reply(target_device_iden, source_user_iden, conversation_iden, message)
 
-            reply(target_device_iden, source_user_iden, conversation_iden, message)
+                    updateSatus(message_id)
+        db.rollback()
+    except pymysql.DatabaseError as e:
+        print(e)
 
-main()
+while True:
+    try: 
+        db = pymysql.connect("localhost", "root", "", "sms_mca")
+        cur = db.cursor()
+        main()
+        print("status: nop \n")
+    except pymysql.DatabaseError as e:
+        print(e)
+    
+    print("\n")
+    time.sleep(5)
+    
+    
+    
+    
+    
